45112f816f369f85f5771d865bc7d0e0afb80f9b
Igor Marinho <igormarinhosilva@gmail.com>
2022-03-26 23:07:39 -0300

    make it better

---
+++ meuSite.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('home.html')
+
+
+# criar nosso template
+
+
+
+
+# rodar o site no server local
+if __name__ =='__main__':
+    app.run(debug=True)
+
+
+

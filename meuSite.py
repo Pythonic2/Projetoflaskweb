6b37ae81f6317849ee8da522d4d967a97c180bf1
Igor Marinho <igormarinhosilva@gmail.com>
2022-04-04 21:44:24 -0300

    alteração do front e do back

--- meuSite.py
+++ meuSite.py
@@ -3,10 +3,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def homepage():
-    return render_template('home.html')
+# @app.route('/')
+# def homepage():
+#     return render_template('home.html')
 
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/post')
+def post():
+    return render_template('post.html')
 if __name__ =='__main__':
     app.run(debug=True)
 

e2ac744fbeb0bdae950b8313dcfe35e237608e19
Igor Marinho <igormarinhosilva@gmail.com>
2022-04-04 22:17:27 -0300

    total

--- meuSite.py
+++ meuSite.py
@@ -14,6 +14,11 @@ def index():
 @app.route('/post')
 def post():
     return render_template('post.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return ('<h1>Ta usando Droga mizera <h1>'), 404
+
 if __name__ =='__main__':
     app.run(debug=True)
 
